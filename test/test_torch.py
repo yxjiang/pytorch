e5fd5f7c75f42cb4dd7c904e493d731c4ed79dcc
Brian Vaughan <bvaughan@fb.com>
2019-11-13 17:32:13 -0500

    Fix bug in atomicAdd for int16_t
    ghstack-source-id: f1a08b4fe769b030d5a85caeae6f2901a2d4c3bb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29231

--- test/test_torch.py
+++ test/test_torch.py
@@ -2620,6 +2620,26 @@ class _TestTorchMixin(object):
             dest2[idx[i]] = dest2[idx[i]] + src[i]
         self.assertEqual(dest, dest2)
 
+    # add coverage for issue with atomic add that appeared only for 
+    # specific dtypes on cuda:
+    # https://github.com/pytorch/pytorch/issues/29153
+    def test_dtypes_for_index_add(self):
+        def is_signed(dtype):
+            return torch.is_signed(torch.tensor([], dtype=dtype))
+        for device in torch.testing.get_all_device_types():
+            for dtype in torch.testing.get_all_math_dtypes(device):
+                size = [5, 5]
+                if dtype.is_floating_point:
+                    tensor = torch.rand(size, dtype=dtype, device=device)
+                elif is_signed(dtype):
+                    tensor = torch.randint(-5, 15, size, dtype=dtype, device=device)
+                else:
+                    tensor = torch.randint(0, 10, size, dtype=dtype, device=device)
+                # index_add calls atomicAdd on cuda.
+                empty = torch.zeros(size, dtype=dtype, device=device)
+                added = empty.index_add(0, torch.arange(0, size[0], dtype=torch.long, device=device), tensor)
+                self.assertEqual(added, tensor)
+
     def test_t(self):
         # Test 0D tensors
         x = torch.randn(())
@@ -13443,7 +13463,6 @@ class TestTorchDeviceType(TestCase):
         concat_list.append(torch.ones((SIZE2, 1024 * 512), dtype=torch.uint8, device=device))
         result = torch.cat(concat_list)
         self.assertEqual(result.size(0), SIZE1 + SIZE2)
-
 
 # Tests that compare a device's computation with the (gold-standard) CPU's.
 class TestDevicePrecision(TestCase):
