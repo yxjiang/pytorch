82f34d6a242283ed202ecb5119cc58abd3eee1ab
Shihao Xu <xsh6528@gmail.com>
2019-11-13 15:23:13 -0800

    Add `RpcAgentOptions` struct type, which bundles different required arguments for different `RpcAgent`s
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29443
    
    https://github.com/pytorch/pytorch/pull/28226 introduced `worker_to_id` arg to the `def init_model_parallel` function for other `RpcAgent`. While it's not really used by `ProcessGroupAgent`. Cleanup is wanted for this, as described in #29031.
    
    To adapt to the difference of different `RpcAgent`, adding a `RpcAgentOptions` base classes, which allow leveraging inheritance to add extra fields.
    
    closes #29031
    ghstack-source-id: 93865127
    
    Differential Revision: [D18283849](https://our.internmc.facebook.com/intern/diff/D18283849/)

--- test/distributed/rpc/rpc_agent_test_fixture.py
+++ test/distributed/rpc/rpc_agent_test_fixture.py
@@ -23,6 +23,16 @@ class RpcAgentTestFixture(object):
             "self.rpc_backend_name property is required to be implemented."
         )
 
+    @property
+    def rpc_agent_options(self):
+        # This is an abstract test suite class,
+        # different RpcAgent has different types of RpcAgentOptions.
+        raise NotImplementedError(
+            (
+                "self.rpc_agent_options property not implemented for {rpc_backend}."
+            ).format(rpc_backend=self.rpc_backend)
+        )
+
     def test_backend_selected(self):
         # Make sure the correct backend is selected from the registry.
         self.assertEqual(self.rpc_backend.name, self.rpc_backend_name)
